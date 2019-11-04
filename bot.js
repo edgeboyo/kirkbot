649f2b607f5c2d47ea5a72eb2c44e061393a743d
edgeboyo <mkolybko@gmail.com>
2019-11-04 23:11:02 +0000

    Repairing purge

--- bot.js
+++ bot.js
@@ -120,20 +120,26 @@ client.on("message", async message => {
     message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
   }
   
-  if(command === "purgeThatBitchGood") {
+  if(command === "purge") {
     // This command removes all messages from all users in the channel, up to 100.
     
-    // get the delete count, as an actual number.
+    if(message.member.hasPermission("ADMINISTRATOR")){
+        await message.channel.send("Oh, it's XX:30. Yeah you can go. Goodbye");
+        client.destroy();
+        process.exit(1);
+	// get the delete count, as an actual number.
     const deleteCount = parseInt(args[0], 10);
-    
+
     // Ooooh nice, combined conditions. <3
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
+
     // So we get our messages, and delete them. Simple enough, right?
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+
+    }
   }
 
   if(command == "shutdown") {
