f49ef03c863998c7a8f6820edb6cdfa95bf49608
comp500 <comp500@users.noreply.github.com>
2019-11-05 15:09:02 +0000

    Remove s if it's only in one server

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on("ready", () => {
 	console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
 	// Example of changing the bot's playing game to something useful. `client.user` is what the
 	// docs refer to as the "ClientUser".
-	client.user.setActivity(`Serving ${client.guilds.size} servers`);
+	client.user.setActivity(`Serving ${client.guilds.size} server${client.guids.size == 1 ? "" : "s"}`);
 	var allGuilds = Array.from(client.guilds.values());
 	for(var i = 0; i < allGuilds.length; i++){
 		var guild = allGuilds[i];
@@ -28,13 +28,13 @@ client.on("ready", () => {
 client.on("guildCreate", guild => {
 	// This event triggers when the bot joins a guild.
 	console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-	client.user.setActivity(`Serving ${client.guilds.size} servers`);
+	client.user.setActivity(`Serving ${client.guilds.size} server${client.guids.size == 1 ? "" : "s"}`);
 });
 
 client.on("guildDelete", guild => {
 	// this event triggers when the bot is removed from a guild.
 	console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-	client.user.setActivity(`Serving ${client.guilds.size} servers`);
+	client.user.setActivity(`Serving ${client.guilds.size} server${client.guids.size == 1 ? "" : "s"}`);
 });
 
 
