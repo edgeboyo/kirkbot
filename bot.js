944a79a8e40e35c751370fd7f5eeb5597e84fba9
edgeboyo <mkolybko@gmail.com>
2019-11-06 13:52:52 +0000

    Fixed typo

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on("ready", () => {
 	console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
 	// Example of changing the bot's playing game to something useful. `client.user` is what the
 	// docs refer to as the "ClientUser".
-	client.user.setActivity(`Serving ${client.guilds.size} server${client.guids.size == 1 ? "" : "s"}`);
+	client.user.setActivity(`Serving ${client.guilds.size} server${client.guilds.size == 1 ? "" : "s"}`);
 	var allGuilds = Array.from(client.guilds.values());
 	for(var i = 0; i < allGuilds.length; i++){
 		var guild = allGuilds[i];
