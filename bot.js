ff950f2ecf367fd597ba0f6b83c885f8581a4366
comp500 <comp500@users.noreply.github.com>
2019-11-06 15:15:02 +0000

    Fix the typo again (I copy/pasted it!!)

--- bot.js
+++ bot.js
@@ -28,13 +28,13 @@ client.on("ready", () => {
 client.on("guildCreate", guild => {
 	// This event triggers when the bot joins a guild.
 	console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-	client.user.setActivity(`Serving ${client.guilds.size} server${client.guids.size == 1 ? "" : "s"}`);
+	client.user.setActivity(`Serving ${client.guilds.size} server${client.guilds.size == 1 ? "" : "s"}`);
 });
 
 client.on("guildDelete", guild => {
 	// this event triggers when the bot is removed from a guild.
 	console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-	client.user.setActivity(`Serving ${client.guilds.size} server${client.guids.size == 1 ? "" : "s"}`);
+	client.user.setActivity(`Serving ${client.guilds.size} server${client.guilds.size == 1 ? "" : "s"}`);
 });
 
 
