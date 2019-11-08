5a5c1fe37eeeccd35d1806c148077eb2c9d9beb7
comp500 <comp500@users.noreply.github.com>
2019-11-08 17:09:40 +0000

    Stop @everyones for those who don't have permission

--- commands/kirk.ts
+++ commands/kirk.ts
@@ -4,6 +4,10 @@ export default async function(message: Discord.Message, client: Discord.Client, 
 	// makes the bot say something and delete the message. As an example, it's open to anyone to use.
 	// To get the "message" itself we join the `args` back into a string with spaces:
 	const sayMessage = args.join(" ");
+	if ((sayMessage.includes("@everyone") || sayMessage.includes("@here")) && !message.member.hasPermission("ADMINISTRATOR")) {
+		message.delete().catch(O_o => {});
+		return;
+	}
 	// Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
 	message.delete().catch(O_o => {});
 	// And we get the bot to say the thing:
