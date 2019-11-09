7a4ad71628639629d4b3378041ac17526ca4acb5
Newbuntu <mkolybko@gmail.com>
2019-11-09 17:47:18 +0000

    Added a help message (fixes #14)

--- commands/help.ts
+++ commands/help.ts
@@ -1,5 +1,23 @@
 import * as Discord from "discord.js";
 
+function getRandomInt(max: number): number {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 export default async function(message: Discord.Message, client: Discord.Client, args: string[]) {
-	await message.author.send("THIS IS WHERE THE HELP WILL BE SENT");
+	const whoAmI : string[] = ["God", "lecturer"];
+	var chance : number = getRandomInt(whoAmI.length);
+	const helloMessage : string = "Hello I'm KirkBot but you might know me as your " + whoAmI[chance] + `
+									I'm a ChatSoc bot, that's here to help you with anything.
+									Aside from moderating and organising stuff I also have a few commands that you might want to use!
+									Here's the list:
+										* !help - I think you already know what this one does :blush:
+										* !kirk <text> - use this to make me say ***ANYTHING*** <:bicc_kirk:631859177286795324>
+										* !ping - see delay information
+									But my devs are working hard to bring you more.
+									If you want to see how I work here's a link:
+									https://github.com/edgeboyo/kirkbot/
+									Have fun and get ready for a lecture :sunglasses:
+	`.replace(/\t/g, '').replace(/\n\*/g, "\n	*");
+	await message.author.send(helloMessage);
 }
