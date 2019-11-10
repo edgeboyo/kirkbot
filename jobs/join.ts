dfcddec8ca7122d5f5a8c8cec48823309716492e
comp500 <comp500@users.noreply.github.com>
2019-11-10 20:08:56 +0000

    Multiple welcome messages, deduplication of messages

--- jobs/join.ts
+++ jobs/join.ts
@@ -1,14 +1,25 @@
 import * as Discord from "discord.js";
 
+const welcomeMessages = [
+	"OMG! I'm alive!"
+];
+
 export default {
 	ready: async function(client: Discord.Client) {
-		var allGuilds = Array.from(client.guilds.values());
-		for (var i = 0; i < allGuilds.length; i++) {
-			var guild = allGuilds[i];
-			var chnl = guild.channels.find(
+		let allGuilds = Array.from(client.guilds.values());
+		await Promise.all(allGuilds.map(async (guild) => {
+			let chan = guild.channels.find(
 				channel => channel.name == "general" && channel.type == "text"
 			) as Discord.TextChannel;
-			chnl.send("OMG! I'm alive!");
-		}
+			try {
+				let msgs = await chan.fetchMessages({ limit: 5 });
+				let msg = msgs.filter(msg => msg.author.id == client.user.id).find(msg => welcomeMessages.includes(msg.content));
+				// If a welcome message has been posted by the bot in the last 5 messages, return
+				if (msg != null) {
+					return;
+				}
+			} catch {} // Ignore all exceptions
+			chan.send(welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)]);
+		}));
 	}
 };
