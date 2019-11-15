5541b2a5dc85f4f4f631108314fcc4dc3675749e
Newbuntu <mkolybko@gmail.com>
2019-11-15 05:22:25 +0000

    Multiple goodbye support (fixes #17)

--- commands/shutdown.ts
+++ commands/shutdown.ts
@@ -1,11 +1,13 @@
 import * as Discord from "discord.js";
 
+const farewellMessages = ["Oh, it's XX:30. Yeah you can go. Goodbye", "I see. I'll leave you be."];
+
 export default async function(message: Discord.Message, client: Discord.Client, args: string[]) {
 	if (
 		message.member.hasPermission("ADMINISTRATOR") &&
 		(message.guild.id == "629265673666822144" || message.guild.id == "634026138527596554")
 	) {
-		await message.channel.send("Oh, it's XX:30. Yeah you can go. Goodbye");
+		await message.channel.send(farewellMessages[Math.floor(Math.random() * farewellMessages.length)]);
 		client.destroy();
 		process.exit(0);
 	} else {
