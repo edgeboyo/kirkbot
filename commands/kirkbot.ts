6f2722c31be5f7a953e077a0a7198a1469446205
edgeboyo <mkolybko@gmail.com>
2020-03-09 00:32:59 +0000

    Init for the deadlines handler

---
+++ commands/kirkbot.ts
@@ -0,0 +1,39 @@
+import * as Discord from "discord.js";
+
+export default async function(message: Discord.Message, client: Discord.Client, args: string[]) {
+	// makes the bot say something and delete the message. As an example, it's open to anyone to use.
+	// To get the "message" itself we join the `args` back into a string with spaces:
+
+	if (args[0] === "init"){
+		if(args.length < 2){
+			message.channel.send("No channel provided!");
+			return;
+		}
+
+		var config = {
+			"channelID" : args[1],
+			"deadlineinfo": []
+		};
+
+		var confstr = JSON.stringify(config);
+		var fs = require('fs');
+
+		const fileName : string = message.guild.id + ".json";
+		fs.writeFile(fileName, confstr);
+	}
+	
+	/*
+	const sayMessage = args.join(" ");
+	if (
+		(sayMessage.includes("@everyone") || sayMessage.includes("@here")) &&
+		!message.member.hasPermission("ADMINISTRATOR")
+	) {
+		message.delete().catch(O_o => {});
+		return;
+	}
+	// Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+	message.delete().catch(O_o => {});
+	// And we get the bot to say the thing:
+	message.channel.send(sayMessage);
+	*/
+}
