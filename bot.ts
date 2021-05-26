f43823eeef1ce3cff39085c140f3df09b67719b4
edgeboyo <mkolybko@gmail.com>
2021-05-26 14:38:43 +0200

    Some partials fixes

--- bot.ts
+++ bot.ts
@@ -38,11 +38,11 @@ client.on("message", async message => {
 
 	// It's good practice to ignore other bots. This also makes your bot ignore itself
 	// and not get into a spam loop (we call that "botception").
-	if (message.author.bot) return;
+	if (message.author == null || message.author.bot) return;
 
 	// Also good practice to ignore any message that does not start with our prefix,
 	// which is set in the configuration file.
-	if (message.content.indexOf(config.prefix) !== 0) return;
+	if (message.content == null || message.content.indexOf(config.prefix) !== 0) return;
 
 	// Here we separate our "command" name, and our "arguments" for the command.
 	// e.g. if we have the message "+say Is this the real life?" , we'll get the following:
@@ -57,6 +57,8 @@ client.on("message", async message => {
 	if (command == undefined) {
 		return;
 	}
+
+	if(message.partial) return;
 
 	await runCommand(command, message, client, args);
 });
