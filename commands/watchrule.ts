1a5ccfcd1ad9492dca99074e66fc6e53945405eb
edgeboyo <mkolybko@gmail.com>
2021-09-17 21:25:28 +0200

    Corrected filename

--- commands/watchrule.ts
+++ commands/watchrule.ts
@@ -2,13 +2,10 @@ import * as Discord from "discord.js";
 import { addNewRule } from "../jobs/watch";
 
 export default async function(message: Discord.Message, client: Discord.Client, args: string[]) {
-	console.log(args);
 	if (message.member == null || message.guild == null || message.mentions.members == null || args.length !== 2)
 		return;
 
 	const resolvedRole = await message.guild.roles.fetch(args[1]);
-
-	message.channel.send("Setting up...");
 
 	if (resolvedRole === null) {
 		message.channel.send("Not a valid role :/");
