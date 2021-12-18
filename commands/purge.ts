5ce362dd65a326c918515d721e4c2d3601fbe2c0
edgeboyo <45755397+edgeboyo@users.noreply.github.com>
2021-12-18 15:30:17 +0000

    Changed how !purge is handled (#36)
    * Installed developer watch-mode
    
    * Still have problem with ghost messages
    
    * Working purge on message id
    
    * Removed excess comments

--- commands/purge.ts
+++ commands/purge.ts
@@ -5,26 +5,43 @@ export default async function(message: Discord.Message, client: Discord.Client, 
 	if (message.member == null || message.guild == null) return;
 
 	if (message.member.permissions.has("ADMINISTRATOR")) {
-		// get the delete count, as an actual number.
-		const deleteCount = parseInt(args[0], 10);
+		const deletionArgument = args[0];
 
-		// Ooooh nice, combined conditions. <3
-		if (!deleteCount || deleteCount < 2 || deleteCount > 100) {
-			message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-			return;
+		var toDelete;
+		const channel = message.channel;
+
+		if (!(channel instanceof Discord.TextChannel)) return;
+
+		//use the number as a message limit
+		if (deletionArgument.length > 4) {
+			toDelete = await channel.messages.fetch({ after: deletionArgument });
+		} else {
+			// get the delete count, as an actual number.
+			const deleteCount = parseInt(deletionArgument, 10);
+
+			// Ooooh nice, combined conditions. <3
+			if (!deleteCount || deleteCount < 2 || deleteCount > 100) {
+				message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+				return;
+			}
+
+			toDelete = await message.channel.messages.fetch({
+				limit: deleteCount
+			});
 		}
 
-		// So we get our messages, and delete them. Simple enough, right?
-		const fetched = await message.channel.messages.fetch({
-			limit: deleteCount
+		const deleted = await channel.bulkDelete(toDelete, true);
+
+		const deletedArray = Array.from(deleted.values()).map((mes: Discord.Message) => {
+			return mes.id;
 		});
-		/* POSSIBLY DOESN'T WORK ANYMORE
-		message.channel
-			.messages.delete(fetched)
-			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-		*/
-		Array.from(fetched.values()).forEach(function(mes) {
-			message.channel.messages.delete(mes);
+
+		const remaining = Array.from(toDelete.values()).filter((mes: Discord.Message) => {
+			return !deletedArray.includes(mes.id);
+		});
+
+		remaining.forEach(async mes => {
+			await channel.messages.delete(mes);
 		});
 	} else {
 		message.reply("Sorry, you don't have permissions to use this!");
