8da2ff17be3a3d679664b97669b8fb05f089c4cf
edgeboyo <mkolybko@gmail.com>
2021-12-18 15:28:59 +0000

    Removed excess comments

--- commands/purge.ts
+++ commands/purge.ts
@@ -5,8 +5,6 @@ export default async function(message: Discord.Message, client: Discord.Client, 
 	if (message.member == null || message.guild == null) return;
 
 	if (message.member.permissions.has("ADMINISTRATOR")) {
-		// get the delete count, as an actual number.
-
 		const deletionArgument = args[0];
 
 		var toDelete;
@@ -14,9 +12,11 @@ export default async function(message: Discord.Message, client: Discord.Client, 
 
 		if (!(channel instanceof Discord.TextChannel)) return;
 
+		//use the number as a message limit
 		if (deletionArgument.length > 4) {
 			toDelete = await channel.messages.fetch({ after: deletionArgument });
 		} else {
+			// get the delete count, as an actual number.
 			const deleteCount = parseInt(deletionArgument, 10);
 
 			// Ooooh nice, combined conditions. <3
@@ -43,21 +43,6 @@ export default async function(message: Discord.Message, client: Discord.Client, 
 		remaining.forEach(async mes => {
 			await channel.messages.delete(mes);
 		});
-
-		/*
-		// So we get our messages, and delete them. Simple enough, right?
-		const fetched = await message.channel.messages.fetch({
-			limit: deleteCount
-		});
-		/* POSSIBLY DOESN'T WORK ANYMORE
-		message.channel
-			.messages.delete(fetched)
-			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-		
-		Array.from(fetched.values()).forEach(function(mes) {
-			message.channel.messages.delete(mes);
-		});
-		*/
 	} else {
 		message.reply("Sorry, you don't have permissions to use this!");
 		return;
