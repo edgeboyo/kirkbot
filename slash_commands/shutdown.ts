7af8972e32e6a26f36f20dc6469eda7dd661a0e8
edgeboyo <mkolybko@gmail.com>
2022-05-29 03:58:49 +0100

    Added shutdown/restart

--- slash_commands/shutdown.ts
+++ slash_commands/shutdown.ts
@@ -1,11 +1,19 @@
 import { Client, CommandInteraction } from "discord.js";
 
-async function commandName(client: Client, interaction: CommandInteraction) {
-	// do a thing
-	// return will not be used
+const farewellMessages = ["Oh, it's XX:30. Yeah you can go. Goodbye", "I see. I'll leave you be.", "I'm Morbin' Out"];
+
+async function shutdown(client: Client, interaction: CommandInteraction) {
+	if (interaction.member == null || interaction.guild == null) return;
+	if (interaction.memberPermissions?.has("ADMINISTRATOR")) {
+		await interaction.reply(farewellMessages[Math.floor(Math.random() * farewellMessages.length)]);
+		client.destroy();
+		process.exit(0);
+	} else {
+		await interaction.reply({ content: "You can't do that. It's illegal!", ephemeral: true });
+	}
 }
 
 export default {
 	commandData: { name: "shutdown", description: "Turn KirkBot off entirely :(", options: [] },
-	handler: commandName
+	handler: shutdown
 };
