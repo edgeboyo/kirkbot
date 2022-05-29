db9356a2a23a5c45153c884047ec70c57173ff7e
edgeboyo <mkolybko@gmail.com>
2022-05-29 04:07:26 +0100

    Added kirk method

--- slash_commands/kirk.ts
+++ slash_commands/kirk.ts
@@ -1,11 +1,28 @@
 import { Client, CommandInteraction } from "discord.js";
 
-async function commandName(client: Client, interaction: CommandInteraction) {
-	// do a thing
-	// return will not be used
+async function kirk(client: Client, interaction: CommandInteraction) {
+	const message = interaction.options.getString("message", true);
+	const convert = interaction.options.getBoolean("covert");
+	if (convert == null || !convert) {
+		interaction.reply(message);
+	} else {
+		if (interaction.channel == null) {
+			interaction.reply({ content: "Can't use covert in this context", ephemeral: true });
+			return;
+		}
+		await interaction.channel.send(message);
+		interaction.reply({ content: "Sent your message secretly ;)", ephemeral: true });
+	}
 }
 
 export default {
-	commandData: { name: "kirk", description: "Make me say something", options: [] },
-	handler: commandName
+	commandData: {
+		name: "kirk",
+		description: "Make me say something",
+		options: [
+			{ name: "message", type: "STRING", description: "Message for KirkBot to say", required: true },
+			{ name: "covert", type: "BOOLEAN", description: "Set to True to not reveal who sent the message" }
+		]
+	},
+	handler: kirk
 };
