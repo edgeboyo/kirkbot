c89e32d034c5ff0ee8c7b31a4aa8fb3b0f8996fb
edgeboyo <mkolybko@gmail.com>
2022-06-02 11:08:34 +0100

    Added a safeguard for non-text-only channels

--- slash_commands/kirk.ts
+++ slash_commands/kirk.ts
@@ -3,10 +3,11 @@ import { Client, CommandInteraction } from "discord.js";
 async function kirk(client: Client, interaction: CommandInteraction) {
 	const message = interaction.options.getString("message", true);
 	const convert = interaction.options.getBoolean("covert");
+
 	if (convert == null || !convert) {
 		interaction.reply(message);
 	} else {
-		if (interaction.channel == null) {
+		if (interaction.channel == null || !interaction.channel.isText()) {
 			interaction.reply({ content: "Can't use covert in this context", ephemeral: true });
 			return;
 		}
