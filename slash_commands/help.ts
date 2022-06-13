f6dd4cc19e1eb41694e2ca814a520b3264e5b6d6
edgeboyo <45755397+edgeboyo@users.noreply.github.com>
2022-06-13 11:33:33 +0200

    Changed the `/help` message to contain `/` rather than `!`

--- slash_commands/help.ts
+++ slash_commands/help.ts
@@ -19,10 +19,10 @@ async function commandName(client: Client, interaction: CommandInteraction) {
 									I'm a ChatSoc bot, that's here to help you with anything.
 									Aside from moderating and organising stuff I also have a few commands that you might want to use!
 									Here's the list:
-										* !help - I think you already know what this one does :blush:
-										* !kirk <text> - use this to make me say ***ANYTHING*** <:bicc_kirk:631859177286795324>
-										* !ping - see delay information
-										* !pingu - see a nice birb
+										* /help - I think you already know what this one does :blush:
+										* /kirk <text> - use this to make me say ***ANYTHING*** <:bicc_kirk:631859177286795324>
+										* /ping - see delay information
+										* /pingu - see a nice birb
 									But my devs are working hard to bring you more.
 									If you want to see how I work here's a link:
 									https://github.com/edgeboyo/kirkbot/
@@ -31,15 +31,15 @@ async function commandName(client: Client, interaction: CommandInteraction) {
 		.replace(/\t/g, "")
 		.replace(/\n\*/g, "\n\t*");
 	const adminMessage: string = `Oh, I see you're an admin! Let me help you out with a few extra commands
-										* !watch <messageId> - make me watch in on reactions to this message :eyes: 
-										* !watch - view watchers, their rules, their ids, locations and properties
-										* !watchrule <watcherId> <emoji> <roleId> - when I see that *emoji* used, I'll assign this *role* to the reactor :pen_ballpoint:
-										* !unwatchrule <watcherId> <ruleId> - remove a rule from a watcher
-										* !movewatch <watcherId> <messageId> - move a watcher from a message to another one. With all the emojis :smile: 
-										* !endofwatch <watcherId> - remove wa watcher and all its rules
-										* !purge <number> - I'll remove as many messages as you want :file_cabinet:
-										* !purge <messageId> - I'll remove all messaged that were created after this one :newspaper:
-										* !kick, !ban - :wink:
+										* /watch <messageId> - make me watch in on reactions to this message :eyes: 
+										* /watch - view watchers, their rules, their ids, locations and properties
+										* /watchrule <watcherId> <emoji> <roleId> - when I see that *emoji* used, I'll assign this *role* to the reactor :pen_ballpoint:
+										* /unwatchrule <watcherId> <ruleId> - remove a rule from a watcher
+										* /movewatch <watcherId> <messageId> - move a watcher from a message to another one. With all the emojis :smile: 
+										* /endofwatch <watcherId> - remove wa watcher and all its rules
+										* /purge <number> - I'll remove as many messages as you want :file_cabinet:
+										* /purge <messageId> - I'll remove all messaged that were created after this one :newspaper:
+										* /kick, /ban - :wink:
 `
 		.replace(/\t/g, "")
 		.replace(/\n\*/g, "\n\t*");
