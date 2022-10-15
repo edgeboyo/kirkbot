5bf404229b0c0da31f70d3369b7daed19e3845c0
edgeboyo <45755397+edgeboyo@users.noreply.github.com>
2022-10-16 00:43:08 +0100

    Removed text_commands

--- text_commands/pingu.ts
+++
@@ -1,6 +0,0 @@
-import * as Discord from "discord.js";
-
-export default async function(message: Discord.Message, client: Discord.Client, args: string[]) {
-	//bot sends picture
-	message.channel.send({ files: ["pic/pingu.png"] });
-}
