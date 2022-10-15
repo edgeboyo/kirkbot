1488ad98b0c404a3bb7db15f9c9e167c520d39ef
edgeboyo <45755397+edgeboyo@users.noreply.github.com>
2022-10-16 01:48:58 +0200

    Removed covert option from /kirk

--- slash_commands/kirk.ts
+++ slash_commands/kirk.ts
@@ -2,18 +2,13 @@ import { Client, CommandInteraction } from "discord.js";
 
 async function kirk(client: Client, interaction: CommandInteraction) {
 	const message = interaction.options.getString("message", true);
-	const convert = interaction.options.getBoolean("covert");
 
-	if (convert == null || !convert) {
-		interaction.reply(message);
-	} else {
-		if (interaction.channel == null || !interaction.channel.isText()) {
-			interaction.reply({ content: "Can't use covert in this context", ephemeral: true });
-			return;
-		}
-		await interaction.channel.send(message);
-		interaction.reply({ content: "Sent your message secretly ;)", ephemeral: true });
+	if (interaction.channel == null || !interaction.channel.isText()) {
+		interaction.reply({ content: "Can't use kirk in this context", ephemeral: true });
+		return;
 	}
+	await interaction.channel.send(message);
+	interaction.reply({ content: "Sent your message ;)", ephemeral: true });
 }
 
 export default {
@@ -21,8 +16,7 @@ export default {
 		name: "kirk",
 		description: "Make KirkBot say something",
 		options: [
-			{ name: "message", type: "STRING", description: "Message for KirkBot to say", required: true },
-			{ name: "covert", type: "BOOLEAN", description: "Set to True to not reveal who sent the message" }
+			{ name: "message", type: "STRING", description: "Message for KirkBot to say", required: true }
 		]
 	},
 	handler: kirk
