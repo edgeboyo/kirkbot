e06a0aded252e2bf4948b96b753cf1dcae36cf08
edgeboyo <45755397+edgeboyo@users.noreply.github.com>
2022-10-16 01:44:59 +0200

    Removed text_commands requirements

--- bot.ts
+++ bot.ts
@@ -23,7 +23,6 @@ import config from "./auth.json";
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
-import runCommand from "./text_commands";
 import { setupJobs, readyJobs } from "./jobs";
 import { setupCommands, validateCommands } from "./slash_commands";
 
@@ -50,35 +49,35 @@ client.on("guildDelete", guild => {
 	console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 });
 
-client.on("messageCreate", async message => {
-	// This event will run on every single message received, from any channel or DM.
+// client.on("messageCreate", async message => {
+// 	// This event will run on every single message received, from any channel or DM.
 
-	// It's good practice to ignore other bots. This also makes your bot ignore itself
-	// and not get into a spam loop (we call that "botception").
-	if (message.author == null || message.author.bot) return;
+// 	// It's good practice to ignore other bots. This also makes your bot ignore itself
+// 	// and not get into a spam loop (we call that "botception").
+// 	if (message.author == null || message.author.bot) return;
 
-	// Also good practice to ignore any message that does not start with our prefix,
-	// which is set in the configuration file.
-	if (message.content == null || message.content.indexOf(config.prefix) !== 0) return;
+// 	// Also good practice to ignore any message that does not start with our prefix,
+// 	// which is set in the configuration file.
+// 	if (message.content == null || message.content.indexOf(config.prefix) !== 0) return;
 
-	// Here we separate our "command" name, and our "arguments" for the command.
-	// e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-	// command = say
-	// args = ["Is", "this", "the", "real", "life?"]
-	const args = message.content
-		.slice(config.prefix.length)
-		.trim()
-		.split(/ +/g);
-	const command = args.shift()?.toLowerCase();
+// 	// Here we separate our "command" name, and our "arguments" for the command.
+// 	// e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+// 	// command = say
+// 	// args = ["Is", "this", "the", "real", "life?"]
+// 	const args = message.content
+// 		.slice(config.prefix.length)
+// 		.trim()
+// 		.split(/ +/g);
+// 	const command = args.shift()?.toLowerCase();
 
-	if (command == undefined) {
-		return;
-	}
+// 	if (command == undefined) {
+// 		return;
+// 	}
 
-	if (message.partial) return;
+// 	if (message.partial) return;
 
-	await runCommand(command, message, client, args);
-});
+// 	await runCommand(command, message, client, args);
+// });
 
 setupJobs(client).then(() => {
 	client.login(config.token);
